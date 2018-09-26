dd91823cb9b1a313aa278259daddd53f3f8502be
hzobrist <hzobrist@byu.edu>
2018-09-26 15:33:56 -0600

    Took out the camera attempt

--- App.js
+++ App.js
@@ -9,54 +9,54 @@ const logos = {
   "broadmooroutfitters": require("./assets/BroadmoorOutfitters.png"),
   "greenbrieroutfitters": require("./assets/GBO_LOGO.png"),
   "gasparillaadventures": require("./assets/GIA_logov2.png"),
-  "boarsheadoutfitters": require("./assets/resort_outfitters.png")
+  "boarsheadoutfitters": require("./assets/boarshead_outfitters.png")
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      permissionsGranted: null,
-      type: 'front',
-      flash: 'off',
-      autofocus: 'on',
-      photoID: 1,
-      photos: []
+      // permissionsGranted: null,
+      // type: 'front',
+      // flash: 'off',
+      // autofocus: 'on',
+      // photoID: 1,
+      // photos: []
     };
   }
 
 async componentWillMount(){
-  const {status} = await Permissions.askAsync(Permissions.CAMERA);
-  this.setState({permissionsGranted: status === 'granted'});
+  // const {status} = await Permissions.askAsync(Permissions.CAMERA);
+  // this.setState({permissionsGranted: status === 'granted'});
 }
 
   async componentDidMount() {
-    this._getLocationKey();
-    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory+'Smartwaiver/photos').catch(e=> {
-      console.log(e,'Directory exists');
-    });
+    // this._getLocationKey();
+    // FileSystem.makeDirectoryAsync(FileSystem.documentDirectory+'Smartwaiver/photos').catch(e=> {
+    //   console.log(e,'Directory exists');
+    // });
   }
 
-  takePicture = async function() {
-    if(this.camera) {
-      console.log('inside camera');
-      this.camera.takePictureAsync().catch(e=>{console.log(e);}).then(data=> {
-        CameraRoll.saveToCameraRoll(data.uri, 'photo${this.state.photoID}')
-        Console.log(data.uri);
-        Console.log('inside camera roll');
-        //FileSystem.copyAsync({
-          //from: data.uri,
-          //to: '${FileSystem.documentDirectory}photos/Photo_${this.state.photoID}.jpg'
-        }).then(() => {
-          this.setState({
-            photoID: this.state.photoID +1,
-          });
-          Vibration.vibrate();
-        //  });
-        });
-    }
-    console.log('picture taken');
-  }
+  // takePicture = async function() {
+  //   if(this.camera) {
+  //     console.log('inside camera');
+  //     this.camera.takePictureAsync().catch(e=>{console.log(e);}).then(data=> {
+  //       CameraRoll.saveToCameraRoll(data.uri, 'photo${this.state.photoID}')
+  //       Console.log(data.uri);
+  //       Console.log('inside camera roll');
+  //       //FileSystem.copyAsync({
+  //         //from: data.uri,
+  //         //to: '${FileSystem.documentDirectory}photos/Photo_${this.state.photoID}.jpg'
+  //       }).then(() => {
+  //         this.setState({
+  //           photoID: this.state.photoID +1,
+  //         });
+  //         Vibration.vibrate();
+  //       //  });
+  //       });
+  //   }
+  //   console.log('picture taken');
+  // }
 
   render() {
 
@@ -84,16 +84,16 @@ async componentWillMount(){
                   </View>);
               })}
             </ScrollView>
-            <Camera
-              ref={ref=>{
-                this.camera=ref;
-              }}
-        //      aspect ={this.state.camera.aspect}
-        //      captureTarget={this.state.captureTarget}
-              style = {{display:'none'}}
-              type={this.state.type}
-              flashMode={this.state.flashMode}
-            />
+        //     <Camera
+        //       ref={ref=>{
+        //         this.camera=ref;
+        //       }}
+        // //      aspect ={this.state.camera.aspect}
+        // //      captureTarget={this.state.captureTarget}
+        //       style = {{display:'none'}}
+        //       type={this.state.type}
+        //       flashMode={this.state.flashMode}
+        //     />
           </View>
         );
       }
@@ -129,7 +129,7 @@ async componentWillMount(){
   };
 
   _handleButtonPressAsync = (uri) => {
-      this.takePicture();
+      // this.takePicture();
       WebBrowser.openBrowserAsync('https://www.smartwaiver.com/v/'+uri);
   };
 
